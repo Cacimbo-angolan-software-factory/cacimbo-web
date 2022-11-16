f5548833b6d05288a0c954667ab83d015ab987af
shinobi <sipitalie@gmail.com>
2022-11-16 09:45:38 +0100

    url-test

---
+++ src/Service.api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'https://cacimboweb.com/api/',
+});
+
+const apiCreate = axios.create({
+    baseURL:'https://license-cacimbo-app.herokuapp.com/v1/'
+});
+const baseUrlGetNif=axios.create({
+    baseURL:'https://cacimboweb.com/consultarNif/'
+});
+export { api, apiCreate, baseUrlGetNif};
+
