5d4263402c3c4b0aa59c9006a37df3b9a3a26f18
michelpinto <michabbe9@gmail.com>
2022-11-24 14:06:08 +0200

    side bar logic and animation done

--- src/components/sidebar/SideBar.tsx
+++ src/components/sidebar/SideBar.tsx
@@ -1,5 +1,13 @@
 import React, { useContext } from 'react';
-import { IoIosCloseCircleOutline } from 'react-icons/io';
+import { IoCloseCircleOutline } from 'react-icons/io5';
+import { IoPersonOutline } from 'react-icons/io5';
+import { IoBusinessOutline } from 'react-icons/io5';
+import { IoIdCardOutline } from 'react-icons/io5';
+import { IoMailOutline } from 'react-icons/io5';
+import { IoCallOutline } from 'react-icons/io5';
+import { IoLocationOutline } from 'react-icons/io5';
+import { IoKeyOutline } from 'react-icons/io5';
+import { IoGlobeOutline } from 'react-icons/io5';
 import { LicContext } from '../../context';
 
 import { Wrapper } from './sidebarStyles';
@@ -10,16 +18,77 @@ interface SidebarProps {
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ handleClose, empresaSelected }) => {
-  console.log(empresaSelected);
+  const {
+    Nome,
+    Responsavel,
+    email,
+    id,
+    ProvinciaSede,
+    Nif,
+    telefone,
+    website,
+  } = empresaSelected;
 
   return (
     <Wrapper>
       <div>
-        <h1>{empresaSelected.Nome}</h1>
+        <h1>
+          <IoBusinessOutline />
+          {Nome}
+        </h1>
         <span onClick={handleClose}>
-          <IoIosCloseCircleOutline />
+          <IoCloseCircleOutline />
         </span>
       </div>
+
+      {id && (
+        <p>
+          <IoIdCardOutline />
+          {id && id}
+        </p>
+      )}
+
+      {Responsavel && (
+        <p>
+          <IoPersonOutline />
+          {Responsavel && Responsavel}
+        </p>
+      )}
+
+      {email && (
+        <p>
+          <IoMailOutline />
+          {email}
+        </p>
+      )}
+
+      {telefone && (
+        <p>
+          <IoCallOutline />
+          {telefone}
+        </p>
+      )}
+
+      {Nif && (
+        <p>
+          <IoKeyOutline />
+          {Nif}
+        </p>
+      )}
+
+      {ProvinciaSede && (
+        <p>
+          <IoLocationOutline />
+          {ProvinciaSede}
+        </p>
+      )}
+
+      {website && (
+        <p>
+          <IoGlobeOutline />
+          {website}
+        </p>
+      )}
     </Wrapper>
   );
 };
--- src/components/sidebar/sidebarStyles.ts
+++ src/components/sidebar/sidebarStyles.ts
@@ -1,4 +1,14 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const animation = keyframes`
+from {
+      transform: translateX(100%);
+    }
+    to {
+      transform: translateX(0%);
+    }
+  
+`;
 
 export const Wrapper = styled.div`
   position: fixed;
@@ -10,13 +20,35 @@ export const Wrapper = styled.div`
   background-color: #1d1d1d;
   box-shadow: -16px 0px 37px -26px rgba(0, 0, 0, 0.47);
   border-left: 0.5px solid #3a3a3a;
+  animation: ${animation} 0.5s;
 
-  display: flex;
-  justify-content: space-between;
+  & h1 {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+
+    & svg {
+      font-size: 2rem;
+    }
+  }
+
+  & div {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 3rem;
+  }
 
   & span {
     font-size: 2.4rem;
     cursor: pointer;
     height: fit-content;
+  }
+
+  & p {
+    font-size: 1.6rem;
+    margin-bottom: 2rem;
+    display: flex;
+    align-items: center;
+    gap: 1rem;
   }
 `;
