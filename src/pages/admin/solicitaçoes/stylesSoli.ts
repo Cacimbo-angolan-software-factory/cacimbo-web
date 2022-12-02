6cc747cbd161c06609eb06dbb7d18e6f21d956f5
michelpinto <michabbe9@gmail.com>
2022-12-02 10:50:02 +0200

    adding media queries

--- src/pages/admin/solicitaçoes/stylesSoli.ts
+++ src/pages/admin/solicitaçoes/stylesSoli.ts
@@ -3,7 +3,32 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
-  /* grid-template-rows: repeat(4, 1fr); */
-  grid-column-gap: 30px;
+  grid-column-gap: 20px;
   grid-row-gap: 20px;
+
+  @media (max-width: 1200px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (max-width: 800px) {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+export const Button = styled.button`
+  padding: 1.2rem 2rem;
+  border-radius: 0.5rem;
+  border: none;
+  background-color: #b71c50;
+  width: 20rem;
+  font-size: 1.6rem;
+  color: #ebebeb;
+  cursor: pointer;
+  transition: all 0.2s;
+
+  &:hover {
+    transform: translateY(-3px);
+  }
 `;
