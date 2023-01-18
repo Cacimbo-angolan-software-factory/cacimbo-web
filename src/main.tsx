857d3998acc842724ab235544be1fdbef64635a9
michelpinto <michabbe9@gmail.com>
2023-01-18 13:38:56 +0200

    adding the provider to the app

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,19 @@ import App from './App';
 import Layout from './styles/Globals';
 import { LicProvider } from './context/index';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <LicProvider>
-        <Layout>
-          <App />
-        </Layout>
-      </LicProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <LicProvider>
+          <Layout>
+            <App />
+          </Layout>
+        </LicProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
