f06405d8513a51568cf3f43d342b819dcbff022a
michelpinto <michabbe9@gmail.com>
2023-01-24 21:42:01 +0200

    criar-empresa

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Spinner from '../../components/spinner/Spinner';
 
 // import { Container } from './styles';
 
 const Home: React.FC = () => {
-  return <h1>Hello world: Cacimbo software factory</h1>;
+  return (
+    <div>
+      <h1>Hello world: Cacimbo software factory</h1>
+      <Spinner />
+    </div>
+  );
 };
 
 export default Home;
