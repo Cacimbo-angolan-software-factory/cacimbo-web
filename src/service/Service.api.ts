897068e0dbacb9c9ef68ac322ed78a1405fee597
michelpinto <michabbe9@gmail.com>
2023-02-06 10:27:34 +0200

    adding new api

--- src/service/Service.api.ts
+++ src/service/Service.api.ts
@@ -1,14 +1,17 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://cacimboweb.com/api/',
+  baseURL: 'https://cacimboweb.com/api/',
+});
+
+const apiCacimbo = axios.create({
+  baseURL: 'http://cacimboerp.cacimboweb.com/api/',
 });
 
 const apiCreate = axios.create({
-    baseURL:'https://license-cacimbo-app.herokuapp.com/v1/'
+  baseURL: 'https://license-cacimbo-app.herokuapp.com/v1/',
 });
-const baseUrlGetNif=axios.create({
-    baseURL:'https://cacimboweb.com/consultarNif/'
+const baseUrlGetNif = axios.create({
+  baseURL: 'https://cacimboweb.com/consultarNif/',
 });
-export { api, apiCreate, baseUrlGetNif};
-
+export { api, apiCreate, baseUrlGetNif, apiCacimbo };
