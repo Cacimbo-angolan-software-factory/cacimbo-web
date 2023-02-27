44aa528ffd83687ebbbcfd68f93e3aeca48646d1
michelpinto <michabbe9@gmail.com>
2023-02-27 13:01:25 +0200

    checkmarkField component

---
+++ src/components/CheckMarkField.tsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import {
+  OutlinedInput,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  ListItemText,
+  Checkbox,
+} from '@mui/material';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import styled from 'styled-components';
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const FormContainer = styled(FormControl)`
+  & .MuiFormLabel-root {
+    color: white;
+  }
+
+  & #demo-multiple-checkbox {
+    color: white;
+    font-size: 1.3rem;
+    width: 90%;
+    /* padding-right: 5rem; */
+  }
+
+  & label.Mui-focused {
+    color: white;
+  }
+  & label.Mui-active {
+    padding-right: 5px;
+  }
+  & .MuiOutlinedInput-root {
+    background-color: #1d1d1d;
+    border-radius: 0.5rem;
+    height: 5rem;
+    &.Mui-focused fieldset {
+      border-color: white;
+    }
+    &:hover fieldset {
+      border-color: white;
+    }
+    & fieldset {
+      border-color: white;
+    }
+    & svg {
+      color: white;
+    }
+  }
+`;
+
+interface CheckMarkFieldProps {
+  children: React.ReactNode;
+  value: string[];
+  handleChange: (event: SelectChangeEvent<string[]>) => void;
+  tag: string;
+}
+
+const CheckMarkField: React.FC<CheckMarkFieldProps> = ({
+  children,
+  value,
+  handleChange,
+  tag,
+}) => {
+  return (
+    <div>
+      <FormContainer sx={{ maxWidth: '50%' }}>
+        <InputLabel id='demo-multiple-checkbox-label'>{tag}</InputLabel>
+        <Select
+          labelId='demo-multiple-checkbox-label'
+          id='demo-multiple-checkbox'
+          multiple
+          value={value}
+          onChange={handleChange}
+          input={<OutlinedInput label={tag} />}
+          renderValue={(selected) => (selected as any).join(', ')}
+          MenuProps={MenuProps}
+        >
+          {children}
+        </Select>
+      </FormContainer>
+    </div>
+  );
+};
+
+export default CheckMarkField;
