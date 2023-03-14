b3049019236546c85936b449026559d0449c66ab
michelpinto <michabbe9@gmail.com>
2023-03-14 13:11:19 +0200

    styling

--- src/components/solicitacoes/stylesSingleSoli.ts
+++ src/components/solicitacoes/stylesSingleSoli.ts
@@ -11,6 +11,7 @@ export const Container = styled.div`
   gap: 1rem;
   height: 100%;
   max-width: 50rem;
+  justify-content: space-between;
 
   & p {
     color: #6c6c6c;
@@ -22,5 +23,31 @@ export const Container = styled.div`
 
   @media (max-width: 768px) {
     width: 100%;
+  }
+`;
+
+export const Buttons = styled.div`
+  display: flex;
+  gap: 1rem;
+  margin-top: 2rem;
+
+  & button {
+    padding: 1rem;
+    border: none;
+    border-radius: 0.5rem;
+    font-size: 1.6rem;
+    cursor: pointer;
+    display: flex;
+    gap: 0.5rem;
+  }
+
+  & button:first-child {
+    background-color: #b71c50;
+    color: #ebebeb;
+  }
+
+  & button:last-child {
+    background-color: #766868;
+    color: #ebebeb;
   }
 `;
