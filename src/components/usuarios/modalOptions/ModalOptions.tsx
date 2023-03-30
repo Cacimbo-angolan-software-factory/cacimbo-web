536625bfea526232798b3be0801907fd1f007b3b
michelpinto <michabbe9@gmail.com>
2023-03-30 13:27:15 +0200

    adding a options button that opens modal

---
+++ src/components/usuarios/modalOptions/ModalOptions.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container } from './ModalOptionsStyles';
+
+const ModalOptions: React.FC = () => {
+  return (
+    <Container>
+      <p>Perfis</p>
+      <p>Editar perfil</p>
+      <p>testing</p>
+    </Container>
+  );
+};
+
+export default ModalOptions;
