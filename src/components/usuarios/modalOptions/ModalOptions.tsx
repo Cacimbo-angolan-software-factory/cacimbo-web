ee8319636dc76bcf10c420bd65804514a741ce23
michelpinto <michabbe9@gmail.com>
2023-04-05 16:19:23 +0200

    ref to modal

--- src/components/usuarios/modalOptions/ModalOptions.tsx
+++ src/components/usuarios/modalOptions/ModalOptions.tsx
@@ -4,11 +4,12 @@ import { Container } from './ModalOptionsStyles';
 
 interface Props {
   setOpenPerfis: (openPerfis: boolean) => void;
+  modalRef?: React.MutableRefObject<any>;
 }
 
-const ModalOptions: React.FC<Props> = ({ setOpenPerfis }) => {
+const ModalOptions: React.FC<Props> = ({ setOpenPerfis, modalRef }) => {
   return (
-    <Container>
+    <Container ref={modalRef}>
       <p onClick={() => setOpenPerfis(true)}>Perfis</p>
       <p>Editar perfil</p>
       <p>testing</p>
