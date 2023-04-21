b0e5b2e423dd092455ea565f4b077ea2d46a76b2
michelpinto <michabbe9@gmail.com>
2023-04-21 21:33:06 +0200

    adding filters and rendering

---
+++ src/pages/admin/solicitaçoes/FiltersSoli.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import { DivFilters } from './stylesSoli';
+import { RiFilter3Fill } from 'react-icons/ri';
+
+interface FiltersSoliProps {
+  filtro: string;
+  setFiltro: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const FiltersSoli: React.FC<FiltersSoliProps> = ({ filtro, setFiltro }) => {
+  return (
+    <DivFilters>
+      <span
+        className={filtro === 'todas' ? 'active' : ''}
+        onClick={() => setFiltro('todas')}
+      >
+        <h2>Todas</h2>
+      </span>
+      <span
+        className={filtro === 'porAprovar' ? 'active' : ''}
+        onClick={() => setFiltro('porAprovar')}
+      >
+        <h2>Por aprovar</h2>
+      </span>
+      <span
+        className={filtro === 'leilao' ? 'active' : ''}
+        onClick={() => setFiltro('leilao')}
+      >
+        <h2>Em leilão</h2>
+      </span>
+      <span
+        className={filtro === 'atribuidas' ? 'active' : ''}
+        onClick={() => setFiltro('atribuidas')}
+      >
+        <h2>Atribuidas</h2>
+      </span>
+      <RiFilter3Fill />
+    </DivFilters>
+  );
+};
+
+export default FiltersSoli;
--- src/pages/admin/solicitaçoes/stylesSoli.ts
+++ src/pages/admin/solicitaçoes/stylesSoli.ts
@@ -34,3 +34,81 @@ export const InputSearch = styled.input`
     width: 100%;
   }
 `;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 1.5rem;
+  border: 0.5px solid #3a3a3a;
+  background-color: #5555;
+  border-radius: 0.5rem;
+  font-size: 1.6rem;
+  gap: 1rem;
+  height: 100%;
+  max-width: 50rem;
+  justify-content: space-between;
+
+  & p {
+    color: #6c6c6c;
+  }
+
+  & h2.nif {
+    font-weight: 400;
+  }
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+
+  div {
+    display: flex;
+    gap: 1rem;
+    margin-top: 2rem;
+
+    & button {
+      padding: 1rem;
+      border: none;
+      border-radius: 0.5rem;
+      font-size: 1.6rem;
+      cursor: pointer;
+      display: flex;
+      gap: 0.5rem;
+      background-color: #00a300;
+      color: #ebebeb;
+      display: flex;
+      align-items: center;
+    }
+
+    & button.interesse {
+      background-color: #766868;
+      color: #ebebeb;
+    }
+  }
+`;
+
+export const DivFilters = styled.div`
+  display: flex;
+  gap: 2rem;
+  align-items: center;
+
+  & span {
+    background: rgba(56, 56, 56, 0.6);
+    border-radius: 1rem;
+    padding: 0.5rem 1.2rem;
+    cursor: pointer;
+    transition: all 0.2s;
+
+    & h2:hover {
+      color: #b71c50;
+    }
+
+    &.active {
+      border: 0.5px solid #b71c50;
+    }
+  }
+
+  & svg {
+    font-size: 2rem;
+    cursor: pointer;
+  }
+`;
