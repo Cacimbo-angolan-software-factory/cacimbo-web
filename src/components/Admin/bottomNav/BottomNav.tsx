ea5ab2c7bf6695f48cb4fb1bb98b8eab238b20ad
michelpinto <michabbe9@gmail.com>
2023-05-29 13:30:04 +0200

    user dropdown at the everypage

--- src/components/Admin/bottomNav/BottomNav.tsx
+++ src/components/Admin/bottomNav/BottomNav.tsx
@@ -18,9 +18,9 @@ import { AppDispatch } from '../../../redux/store';
 import { Link, useNavigate } from 'react-router-dom';
 
 interface BottomNavProps {
-  fixedNav: boolean;
-  setCriarPermission: (value: boolean) => void;
-  setCriarRole: (value: boolean) => void;
+  fixedNav?: boolean;
+  setCriarPermission?: (value: boolean) => void;
+  setCriarRole?: (value: boolean) => void;
 }
 
 const BottomNav: React.FC<BottomNavProps> = ({
@@ -28,37 +28,18 @@ const BottomNav: React.FC<BottomNavProps> = ({
   setCriarPermission,
   setCriarRole,
 }) => {
-  const [isOpenUser, setIsOpenUser] = React.useState(false);
   const [isOpenSettings, setIsOpenSettings] = React.useState(false);
-  const dispatch = useDispatch<AppDispatch>();
-  const navigate = useNavigate();
   const { user } = useSelector((state: any) => state.user);
   let modalRef = useRef<any>(null);
 
-  const handleLogout = () => {
-    dispatch(logout());
-    navigate('/Login');
-  };
-
   const handleSettings = () => {
     setIsOpenSettings(!isOpenSettings);
-    if (isOpenSettings) {
-      setIsOpenUser(false);
-    }
-  };
-
-  const handleUser = () => {
-    setIsOpenUser(!isOpenUser);
-    if (isOpenUser) {
-      setIsOpenSettings(false);
-    }
   };
 
   useEffect(() => {
     let handler = (event: any) => {
       if (!modalRef.current?.contains(event.target)) {
         setIsOpenSettings(false);
-        setIsOpenUser(false);
       }
     };
 
@@ -78,39 +59,18 @@ const BottomNav: React.FC<BottomNavProps> = ({
         <span>
           <IoListOutline />
         </span>
-        <span onClick={handleUser}>
-          <IoPeopleOutline />
-        </span>
-
-        {isOpenUser && (
-          <Modal modalRef={modalRef}>
-            <ModalItem>
-              <IoPeopleOutline />
-              {user.user.name}
-            </ModalItem>
-
-            <Link to='/NovaPalavraPasse'>
-              <ModalItem>
-                <IoKeyOutline />
-                Mudar palavra-passe
-              </ModalItem>
-            </Link>
-            <ModalItem onClick={handleLogout}>
-              <IoLogOutOutline />
-              Logout
-            </ModalItem>
-          </Modal>
-        )}
 
         {isOpenSettings && (
           <Modal modalRef={modalRef}>
             {user.user.tipo === 'Parceiro' ? null : (
-              <ModalItem onClick={() => setCriarPermission(true)}>
+              <ModalItem
+                onClick={() => setCriarPermission && setCriarPermission(true)}
+              >
                 <IoBuildOutline />
                 Permissões
               </ModalItem>
             )}
-            <ModalItem onClick={() => setCriarRole(true)}>
+            <ModalItem onClick={() => setCriarRole && setCriarRole(true)}>
               <IoPersonAddOutline />
               Funções de usuário
             </ModalItem>
---
+++ src/components/userDropDown/stylesUserDropdown.ts
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div``;
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.8rem;
+  padding: 0.8rem 1.6rem;
+  border-radius: 0.5rem;
+  height: 4rem;
+  font-size: 1.4rem;
+  color: #ebebeb;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  cursor: pointer;
+  position: relative;
+
+  & ul {
+    display: none;
+    position: absolute;
+    top: 4rem;
+    left: 0;
+    padding: 1rem 0;
+    min-width: 200px;
+    border-radius: 0.5rem;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  }
+
+  & li {
+    list-style: none;
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    padding: 0.8rem 1rem;
+    width: 100%;
+
+    &:hover {
+      background-color: #2d2d2d;
+    }
+  }
+
+  &:hover ul {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+`;
+
+export const DropDown = styled.ul``;
