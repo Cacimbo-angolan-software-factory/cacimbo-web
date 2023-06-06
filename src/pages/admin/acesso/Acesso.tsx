5cef8c8b4c2065fc90244b65e101096e858cd2da
michelpinto <michabbe9@gmail.com>
2023-06-06 12:09:10 +0200

    Adding new page Acesso

---
+++ src/pages/admin/acesso/Acesso.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import AdminHeader from '../../../components/adminHeader/AdminHeader';
+import HeaderMobile from '../../../components/headerMobile/HeaderMobile';
+
+// import { Container } from './styles';
+
+const Acesso: React.FC = () => {
+  return (
+    <>
+      <AdminHeader />
+      <HeaderMobile />
+
+      <h1> Hello world</h1>
+    </>
+  );
+};
+
+export default Acesso;
