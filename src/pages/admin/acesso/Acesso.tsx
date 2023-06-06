6330ab6484782d6953c594716d391debc46910c4
michelpinto <michabbe9@gmail.com>
2023-06-06 13:21:37 +0200

    building the tabs

--- src/pages/admin/acesso/Acesso.tsx
+++ src/pages/admin/acesso/Acesso.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import AdminHeader from '../../../components/adminHeader/AdminHeader';
 import HeaderMobile from '../../../components/headerMobile/HeaderMobile';
+import { Container } from './AcessoStyles';
+import AcessoTabs from '../../../components/acesso/AcessoTabs';
 
 // import { Container } from './styles';
 
 const Acesso: React.FC = () => {
+  const [activeTab, setActiveTab] = React.useState('padrao');
+
   return (
     <>
       <AdminHeader />
       <HeaderMobile />
 
-      <h1> Hello world</h1>
+      <Container>
+        <AcessoTabs activeTab={activeTab} setActiveTab={setActiveTab} />
+      </Container>
     </>
   );
 };
