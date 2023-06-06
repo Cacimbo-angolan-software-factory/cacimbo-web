626b6e2044f58fbfe5d8151c0c4f0b82e972bcaa
michelpinto <michabbe9@gmail.com>
2023-06-06 14:24:21 +0200

    styling

--- src/pages/admin/acesso/AcessoStyles.ts
+++ src/pages/admin/acesso/AcessoStyles.ts
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
+  flex-direction: column;
+  gap: 2rem;
   padding: 2rem;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
   background: rgba(56 56, 56, 0.6);
