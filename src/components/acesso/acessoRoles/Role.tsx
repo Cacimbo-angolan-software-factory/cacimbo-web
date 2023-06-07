cb69a81b61df4692fc35f0bef0d4f4d2b843d5e4
michelpinto <michabbe9@gmail.com>
2023-06-07 14:48:50 +0200

    adding permissions and styling it

--- src/components/acesso/acessoRoles/Role.tsx
+++ src/components/acesso/acessoRoles/Role.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 
-import { RoleContainer } from './acessoRolesStyles';
+import { Permission, Permissions, RoleContainer } from './acessoRolesStyles';
 import {
   IoChevronDownOutline,
   IoChevronUpOutline,
   IoInformationCircleOutline,
+  IoAddCircleOutline,
 } from 'react-icons/io5';
 
 interface RoleProps {
@@ -12,7 +13,21 @@ interface RoleProps {
 }
 
 const Role: React.FC<RoleProps> = ({ role }) => {
-  const [isExpanded, setIsExpanded] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(true);
+  const [value, setValue] = useState<any>([]);
+
+  const handleChange = (index: number, subIndex: number, roleIndex: number) => {
+    const newValue: any = [...value];
+    newValue[roleIndex].permissions[index].youCan[subIndex].checked =
+      !newValue[roleIndex].permissions[index].youCan[subIndex].checked;
+    setValue(newValue);
+  };
+
+  const sortedPermissions = [...role.permissions].sort((a, b) => {
+    if (a.name !== '' && b.name === '') return -1;
+    if (a.name === '' && b.name !== '') return 1;
+    return 0;
+  });
 
   return (
     <RoleContainer>
@@ -26,9 +41,60 @@ const Role: React.FC<RoleProps> = ({ role }) => {
       </p>
 
       {isExpanded && (
-        <div>
-          <h3>Permissões</h3>
-        </div>
+        <Permissions>
+          {sortedPermissions.map((permission: any, index: number) => (
+            <>
+              {permission.name !== '' && (
+                <Permission key={index}>
+                  <IoAddCircleOutline />
+                  <div>
+                    <span>
+                      <input
+                        type='checkbox'
+                        id={`switch-permission-${role.id}-${index}`}
+                        checked={value[role.id]?.permissions[index]?.checked}
+                        onChange={() => handleChange(role.id, index, 0)}
+                      />
+                      <label htmlFor={`switch-permission-${role.id}-${index}`}>
+                        Toggle
+                      </label>
+                    </span>
+                    <p>{permission.name}</p>
+                    <button>Editar</button>
+                  </div>
+                </Permission>
+              )}
+              {permission.name === '' &&
+                permission.youCan.map((youCanItem: any, subIndex: number) => (
+                  <Permission key={subIndex}>
+                    {permission.name === '' && !(<IoAddCircleOutline />)}
+                    <div className='youCan'>
+                      <span>
+                        <input
+                          type='checkbox'
+                          id={`switch-youcan-${role.id}-${index}-${subIndex}`}
+                          checked={
+                            value[role.id]?.permissions[index]?.youCan[subIndex]
+                              ?.checked
+                          }
+                          onChange={() =>
+                            handleChange(role.id, index, subIndex)
+                          }
+                        />
+                        <label
+                          htmlFor={`switch-youcan-${role.id}-${index}-${subIndex}`}
+                        >
+                          Toggle
+                        </label>
+                      </span>
+                      <p>{youCanItem}</p>
+                      <button>Editar</button>
+                    </div>
+                  </Permission>
+                ))}
+            </>
+          ))}
+        </Permissions>
       )}
     </RoleContainer>
   );
--- src/components/acesso/acessoRoles/acessoRolesStyles.ts
+++ src/components/acesso/acessoRoles/acessoRolesStyles.ts
@@ -41,3 +41,102 @@ export const RoleContainer = styled.div`
     color: #6c6c6c;
   }
 `;
+
+export const Permissions = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+  grid-column-gap: 2rem;
+  margin: 2.5rem 0 0 2.5rem;
+  gap: 1.5rem;
+`;
+
+export const Permission = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+
+  & div {
+    background-color: #3d3d3d;
+    padding: 0.8rem;
+    border-radius: 0.5rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 0.5rem;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+    min-width: 20.5rem;
+
+    & span {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      & input {
+        height: 0;
+        width: 0;
+        visibility: hidden;
+      }
+
+      & label {
+        cursor: pointer;
+        text-indent: -9999px;
+        width: 4rem;
+        height: 2rem;
+        background: grey;
+        display: block;
+        border-radius: 1rem;
+        position: relative;
+      }
+
+      & label:after {
+        content: '';
+        position: absolute;
+        top: 0.1rem;
+        left: 0.1rem;
+        width: 1.8rem;
+        height: 1.8rem;
+        background: #fff;
+        border-radius: 1rem;
+        transition: 0.3s;
+      }
+
+      & input:checked + label {
+        background: #b71c50;
+      }
+
+      & input:checked + label:after {
+        left: calc(100% - 0.1rem);
+        transform: translateX(-100%);
+      }
+
+      & label:active:after {
+        width: 3rem;
+      }
+    }
+
+    & p {
+      color: #fff;
+      margin-right: 1.6rem;
+      font-size: 1.6rem;
+    }
+
+    & button {
+      background: inherit;
+      border: 1px solid #6c6c6c;
+      padding: 0.5rem;
+      border-radius: 0.5rem;
+      color: #6c6c6c;
+      cursor: pointer;
+    }
+  }
+
+  & div.youCan {
+    margin-left: 3rem;
+  }
+
+  & svg {
+    font-size: 2rem;
+    cursor: pointer;
+  }
+`;
