21f52371572aefb9cd897b71511a3653420e8190
michelpinto <michabbe9@gmail.com>
2023-06-15 14:04:17 +0200

    forms logic

--- src/components/acesso/modalRoles/ModalRoles.tsx
+++ src/components/acesso/modalRoles/ModalRoles.tsx
@@ -7,8 +7,11 @@ import {
   InputDiv,
   Inputs,
   ModalRolesContainer,
+  Overlay,
 } from './modalRolesStyles';
 import { IoCloseCircleOutline } from 'react-icons/io5';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch } from '../../../redux/store';
 
 interface ModalRolesProps {
   openModal: boolean;
@@ -16,38 +19,67 @@ interface ModalRolesProps {
 }
 
 const ModalRoles: React.FC<ModalRolesProps> = ({ openModal, setOpenModal }) => {
+  const { user } = useSelector((state: any) => state.user);
+  const dispatch = useDispatch<AppDispatch>();
+  const [value, setValue] = React.useState({
+    name: '',
+    CompanyID: user.user.lastCompanyIDUsed,
+    description: '',
+    permissions: [] as any,
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue({ ...value, [event.target.name]: event.target.value });
+  };
+
   return (
-    <ModalRolesContainer className={openModal ? 'open' : ''}>
-      <Header>
-        <h2>Criar função</h2>
-        <IoCloseCircleOutline onClick={() => setOpenModal(false)} />
-      </Header>
+    <>
+      <ModalRolesContainer className={openModal ? 'open' : ''}>
+        <Header>
+          <h2>Criar função</h2>
+          <IoCloseCircleOutline onClick={() => setOpenModal(false)} />
+        </Header>
 
-      <Content>
-        <Inputs>
-          <InputDiv>
-            <p>Nome</p>
-            <input type='text' name='name' />
-          </InputDiv>
-          <InputDiv>
-            <p>Descrição</p>
-            <input type='text' name='description' />
-          </InputDiv>
-        </Inputs>
+        <form>
+          <Content>
+            <Inputs>
+              <InputDiv>
+                <p>Nome</p>
+                <input
+                  name='name'
+                  value={value.name}
+                  onChange={handleChange}
+                  type='text'
+                />
+              </InputDiv>
+              <InputDiv>
+                <p>Descrição</p>
+                <input
+                  name='description'
+                  value={value.description}
+                  onChange={handleChange}
+                  type='text'
+                />
+              </InputDiv>
+            </Inputs>
 
-        <div>
-          <h2>Permissões</h2>
-          <h2>Permissões</h2>
-          <h2>Permissões</h2>
-          <h2>Permissões</h2>
-        </div>
-      </Content>
+            <div>
+              <h2>Permissões</h2>
+              <h2>Permissões</h2>
+              <h2>Permissões</h2>
+              <h2>Permissões</h2>
+            </div>
+          </Content>
 
-      <FooterDiv>
-        <button onClick={() => setOpenModal(false)}>Cancelar</button>
-        <button>Criar função</button>
-      </FooterDiv>
-    </ModalRolesContainer>
+          <FooterDiv>
+            <button onClick={() => setOpenModal(false)}>Cancelar</button>
+            <button type='submit'>Criar função</button>
+          </FooterDiv>
+        </form>
+      </ModalRolesContainer>
+
+      {openModal && <Overlay onClick={() => setOpenModal(false)} />}
+    </>
   );
 };
 
