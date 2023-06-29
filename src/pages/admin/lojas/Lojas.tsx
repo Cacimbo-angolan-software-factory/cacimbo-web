2897f8daeb2957386fbff02364fa9c4d5eb0b1b9
michelpinto <michabbe9@gmail.com>
2023-06-29 13:39:10 +0200

    adding the button to create lojas

--- src/pages/admin/lojas/Lojas.tsx
+++ src/pages/admin/lojas/Lojas.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 
-import { Container } from './lojasStyles';
+import { BtnCreate, Container } from './lojasStyles';
 import AdminHeader from '../../../components/adminHeader/AdminHeader';
 import HeaderMobile from '../../../components/headerMobile/HeaderMobile';
+import { IoAddCircleOutline } from 'react-icons/io5';
 
 const Lojas: React.FC = () => {
+  const [showModal, setShowModal] = React.useState(false);
+
   return (
     <>
       <AdminHeader />
       <HeaderMobile />
+
       <Container>
-        <h1>Hello world</h1>
+        <BtnCreate>
+          <IoAddCircleOutline />
+          Criar Loja
+        </BtnCreate>
+
+        <h1>hello world</h1>
       </Container>
     </>
   );
