19868bfceb483238b75ffc0cd6d37a146ca79248
michelpinto <michabbe9@gmail.com>
2023-06-30 12:52:23 +0200

    changing login api to receive companies and roles

--- src/redux/userFeatures/userService.ts
+++ src/redux/userFeatures/userService.ts
@@ -1,11 +1,14 @@
-import { api } from '../../service/Service.api';
+import { api, apiCacimbo } from '../../service/Service.api';
 
 const login = async (userData: {
   email: string;
   password: string;
   device_name: string;
 }) => {
-  const response = await api.post(`token`, { ...userData, device_name: 'web' });
+  const response = await apiCacimbo.post(`token`, {
+    ...userData,
+    device_name: 'web',
+  });
 
   if (response.data) {
     localStorage.setItem('user', JSON.stringify(response.data));
