8291b77727742050282bf234e99f7a09502f4cfa
michelpinto <michabbe9@gmail.com>
2023-07-13 13:07:05 +0200

    button to open modal to choose company roles

--- src/pages/admin/acesso/Acesso.tsx
+++ src/pages/admin/acesso/Acesso.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import AdminHeader from '../../../components/adminHeader/AdminHeader';
 import HeaderMobile from '../../../components/headerMobile/HeaderMobile';
-import { Container } from './AcessoStyles';
+import { Button, Container, Overlay } from './AcessoStyles';
 import AcessoRoles from '../../../components/acesso/acessoRoles/AcessoRoles';
 import ModalRoles from '../../../components/acesso/modalRoles/ModalRoles';
 import { IoAddCircleOutline } from 'react-icons/io5';
+import ModalAcessoEmpresas from '../../../components/acesso/modalAcessoEmpresas/ModalAcessoEmpresas';
 
 const Acesso: React.FC = () => {
   const [openModal, setOpenModal] = React.useState(false);
+  const [openModalEmpresas, setOpenModalEmpresas] = React.useState(false);
 
   return (
     <>
@@ -23,10 +25,20 @@ const Acesso: React.FC = () => {
           </button>
         </div>
 
+        <Button onClick={() => setOpenModalEmpresas(true)}>
+          Escolha uma empresa
+        </Button>
+
         <AcessoRoles />
       </Container>
 
       <ModalRoles openModal={openModal} setOpenModal={setOpenModal} />
+      {openModalEmpresas && (
+        <ModalAcessoEmpresas openModalEmpresas={openModalEmpresas} />
+      )}
+      {openModalEmpresas && (
+        <Overlay onClick={() => setOpenModalEmpresas(false)} />
+      )}
     </>
   );
 };
