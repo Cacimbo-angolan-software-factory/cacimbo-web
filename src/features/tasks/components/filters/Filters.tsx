c90bcf3a23b40b0d5ebd0ebc00fc54f7732a7fb8
michelpinto <michabbe9@gmail.com>
2023-08-28 15:34:55 +0200

    tarefas progress

---
+++ src/features/tasks/components/filters/Filters.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Header } from './filtersStyles';
+
+// import { Container } from './styles';
+
+interface Props {
+  filtro: string;
+  setFiltro: (filtro: string) => void;
+}
+
+const Filters: React.FC<Props> = ({ filtro, setFiltro }) => {
+  return (
+    <Header>
+      <nav>
+        <button
+          className={filtro === 'all' ? 'active' : ''}
+          onClick={() => setFiltro('all')}
+        >
+          Todas
+        </button>
+        <button
+          className={filtro === 'pendentes' ? 'active' : ''}
+          onClick={() => setFiltro('pendentes')}
+        >
+          Pendentes
+        </button>
+        <button
+          className={filtro === 'concluidas' ? 'active' : ''}
+          onClick={() => setFiltro('concluidas')}
+        >
+          Concluídas
+        </button>
+        <input type='text' placeholder='Pesquisar' />
+      </nav>
+      <div>
+        <p>
+          <span className='red'></span> Prioridade alta
+        </p>
+        <p>
+          <span className='green'></span> Prioridade normal
+        </p>
+        <p>
+          <span className='blue'></span> Prioridade baixa
+        </p>
+      </div>
+    </Header>
+  );
+};
+
+export default Filters;
